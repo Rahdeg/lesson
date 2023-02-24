d4d8a5bb6d6b1d60ab3e8946f69da95d1fb6bac6
Rahdeg <walett95@gmail.com>
2023-02-24 14:38:38 +0100

    ups

--- keep.js
+++ keep.js
@@ -274,3 +274,44 @@ export function applyMondayBonus(scoreBoard) {
 export function normalizeScore(params) {
   return params.normalizeFunction(params.score);
 }
+
+interface Planet {
+  [key:strring]: number
+}
+const ratio: Planet={
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
+
+export function age(planet: string, seconds: number): number {
+  return Number((seconds / 31557600 / RATIOS[planet]).toFixed(2))
+}
+///////////////////////////////
+export function createVisitor(name, age, ticketId) {
+  return {
+    name,
+    age,
+    ticketId,
+  }
+}
+//////////////////////////////////////
+export function revokeTicket(visitor) {
+  visitor.ticketId = null;
+ return visitor
+}
+////////////////////////////////////////
+export function ticketStatus(tickets, ticketId) {
+  return tickets[ticketId] === null ? 'not sold': tickets[ticketId]? `sold to ${tickets[ticketId]}` : 'unknown ticket id';
+ }
+
+ //////////////////////////////////////
+ export function simpleTicketStatus(tickets, ticketId) {
+  return tickets[ticketId] ?? 'invalid ticket !!!';
+}
+////////////////////////////////////////////
